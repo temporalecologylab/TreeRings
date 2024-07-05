test different focus scoring methods, update focus to not write with cv2 (hopefully fix ashlar error) and use variance

code/focus.py
@@ -1,14 +1,14 @@
 import cv2
 import logging as log
+import shutil
+import numpy as np
 
 log.basicConfig(format='%(process)d-%(levelname)s-%(message)s', level=log.INFO)
 
 class Focus:
 
     def __init__(self):
-        # odd numbers only, can be tuned
-        self.kernel_size = 5         # Size of the laplacian window
-        self.blur_size = 5           # How big of a kernel to use for the gaussian blur
+        pass
 
     def find_focus(self, img_pipeline, directory):
         while True:
@@ -18,28 +18,32 @@ class Focus:
                 img_pipeline.task_done()
                 break
                       
-            image_name = image_files[1].split("/")[-1].split("_")
+            image_name = image_files[0].split("/")[-1].split("_")
             extract_row = image_name[1]
             extract_col = image_name[2]
             filename = "focused_{}_{}.tiff".format(extract_row, extract_col) 
-            focused_image = self.best_focused_image(image_files)
-            cv2.imwrite("{}/focused_images/{}".format(directory,filename), focused_image)
+            focused_image_name = self.best_focused_image(image_files)
+            shutil.copy(focused_image_name, "{}/focused_images/{}".format(directory,filename))
             img_pipeline.task_done()
         
+    def compute_variance(self, image):
+        # adapted from macro info at https://imagejdocu.list.lu/macro/normalized_variance
+        mean = np.mean(image, axis=(0,1))
+        width, height,_ = image.shape
+        square_diff = (image - mean)**2
+        b = np.sum(square_diff)
+        normVar = b/(height * width * mean)
+        return normVar
+
     def best_focused_image(self, images):
-        best_image = []
-        best_lap = 0.0
+        best_image_filepath = []
+        best_var = np.array([0,0,0])
 
         for image_name in images:
             image = cv2.imread(image_name)
-            lap = self.compute_laplacian(image)
-            if lap.var() > best_lap:
-                best_lap = lap.var()
-                best_image = image
+            var = self.compute_variance(image)
+            if var.all() > best_var.all():
+                best_image_filepath = image_name
+                best_var = var
 
-        return best_image
-    
-    def compute_laplacian(self,image):
-
-        blurred = cv2.GaussianBlur(image, (self.blur_size,self.blur_size), 0)
-        return cv2.Laplacian(blurred, cv2.CV_64F, ksize=self.kernel_size)            
+        return best_image_filepath     