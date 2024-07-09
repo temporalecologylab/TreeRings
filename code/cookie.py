30348ba71ffb5cc9c44bc954abc04acb19027975
Adam Fong <adamfong888@gmail.com>
2024-07-09 16:24:55 -0700

    Getting rid of needless files. Updating cookie class to allow for storage of absolute coordinates

--- code/cookie.py
+++ code/cookie.py
@@ -1,9 +1,13 @@
 class Cookie:
-    def __init__(self, cookie_width_mm: int, cookie_height_mm: int, percent_overlap:int = 20):
+    def __init__(self, cookie_width_mm: int, cookie_height_mm: int, percent_overlap:int = 20, x:float = None, y:float = None, z:float = None):
         self.width = cookie_width_mm
         self.height = cookie_height_mm
         self.percent_overlap = percent_overlap
-        self.start_point = (None, None)
+        self.x = x
+        self.y = y
+        self.z = z
 
-    def set_location(self, top_left: tuple[float, float]):
-        self.start_point = top_left
+    def set_location(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
