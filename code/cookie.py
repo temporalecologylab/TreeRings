a0ba5754274d6b5bde494dd37d14d5ea5b0d4e81
Elizabeth Wolkovich <adamfong888@gmail.com>
2024-09-13 13:17:32 -0700

    Adding docstrings

--- code/cookie.py
+++ code/cookie.py
@@ -7,6 +7,22 @@ from datetime import datetime
 
 class Cookie:
     def __init__(self, cookie_width_mm: int, cookie_height_mm: int, species:str, id1:str, id2:str, notes:str, image_width_mm:float, image_height_mm:float, percent_overlap:int = 50, x:float = None, y:float = None, z:float = None):
+        """Class which contains the necessary information for each sample that needs to be digitized. Data comes from user inputs from GUI. Data is saved in metadata.json files alongside stitched images
+
+        Args:
+            cookie_width_mm (int): Width of minimum bounding rectangle around edges of the sample
+            cookie_height_mm (int): Height of minimum bounding rectangle around the edges of the sample
+            species (str): Species descriptor
+            id1 (str): Additional species ID1
+            id2 (str): Additional specied ID 2
+            notes (str): Additional notes for ambiguity in sample
+            image_width_mm (float): Width in mm of the field of view of a single image. Helpful to print a 1mm x 1mm grid to measure
+            image_height_mm (float): Height in mm of the field of view of a single image. Helpful to print a 1mm x 1mm grid to measure
+            percent_overlap (int, optional): Desired overlap between two images. Defaults to 50.
+            x (float, optional): X position of the center of the sample in the gantry coordinate system. Defaults to None.
+            y (float, optional): Y position of the center of the sample in the gantry coordinate system. Defaults to None.
+            z (float, optional): Z position of the center of the sample in the gantry coordinate system. Defaults to None.
+        """
         self.width = cookie_width_mm
         self.height = cookie_height_mm
         self.percent_overlap = percent_overlap
@@ -35,6 +51,14 @@ class Cookie:
         self.nvar = np.zeros((self.rows, self.cols, 9))
 
     def calculate_grid(self):
+        """Calculates the amount of rows and columns necessary to traverse the cookie.
+
+        Returns:
+            int: Count of rows
+            int: Count of cols
+            float: step size for translation in x direction 
+            float: step size for translation in y direction 
+        """
         overlap_x = round(self.image_width_mm * self.percent_overlap / 100, 3)
         overlap_y = round(self.image_height_mm * self.percent_overlap / 100, 3)
 
@@ -45,16 +69,20 @@ class Cookie:
         rows = math.ceil(self.height / y_step_size)
 
         return rows, cols, x_step_size, y_step_size
-    
-    def set_center_location(self, x, y, z):
-        self._center = (x,y,z)
-
-    def set_top_left_location(self, x, y, z):
-        self._top_left = (x,y,z)
 
     def get_center_location(self):
+        """Retrieve the center location of the sample
+
+        Returns:
+            tuple[float]: X, Y, Z coordinates of the center 
+        """
         return self._center
     
     def get_top_left_location(self):
+        """Retrieve the top left location of the sample
+
+        Returns:
+            tuple[float]: X, Y, Z coordintes of top left corner of sample
+        """
         return self._top_left
     
