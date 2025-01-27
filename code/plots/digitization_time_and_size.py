f09116d9c63ad0e245e7924adbc2aba735a423e2
Adam Fong <adamfong888@gmail.com>
2025-01-27 13:12:47 -0800

    Adding times collected from metadata

--- code/plots/digitization_time_and_size.py
+++ code/plots/digitization_time_and_size.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 
 # Fake data points
-time_minutes = [0, 10, 20, 30, 40]  # Time in minutes
-sample_size_mm2 = [5, 15, 30, 50, 80]  # Sample size in mm^2
+time_minutes = [11, 43.6, 94.5, 120.3, 145.3, 149.6, 325.4]  # Time in minutes
+sample_size_mm2 = [660, 1225, 1225, 1517, 1849, 1980, 4200]  # Sample size in mm^2
+stitch_time_minutes = [0.4, 1.29, 2.98, 3.65, 4.32, 5.14, 10.28]
 
 # Create the plot
 plt.figure(figsize=(8, 6))
-plt.plot(time_minutes, sample_size_mm2, marker='o', linestyle='-', color='b')
-
+plt.plot(time_minutes, sample_size_mm2, marker='o', linestyle='-', color='b', label = "Imaging Time")
+plt.plot(stitch_time_minutes, sample_size_mm2, marker = "x", color = 'r', label = "30% Downscale Stitching Time")
 # Add labels and title
 plt.xlabel('Time (minutes)', fontsize=12)
 plt.ylabel('Sample Size (mm$^2$)', fontsize=12)
-plt.title('Time vs Sample Size', fontsize=14)
+plt.title('Time to Digitize Samples by Surface Area', fontsize=14)
 
 # Add grid and legend
 plt.grid(True, linestyle='--', alpha=0.7)
