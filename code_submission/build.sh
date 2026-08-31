efc5cb9f9157165a073a4c65fc4e3de6f013b753
Adam Fong <adamfong888@gmail.com>
2026-08-31 16:05:08 -0700

    Removing anonymized old code submission from repo and using Anonymous GitHub instead recommended by MEE

--- code_submission/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build --build-arg HOST_UID=$(id -u) --build-arg HOST_GID=$(id -g) -t tim_image .
-
--- code_submission/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Grant 777 permissions to /dev/ttyUSB0
-sudo chmod 777 /dev/ttyUSB0
-
-# Run the default command passed to the container
-exec "$@"
-bash
